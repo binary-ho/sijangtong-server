5d0f2562e10cfab5314ca336ca2ed8041dfebc75
binary-ho <dfghcvb11@naver.com>
2022-09-05 19:18:21 +0900

    test commit for ci/cd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,10 @@ REPOSITORY=/home/ec2-user/app/step1
 PROJECT_NAME=sijangtong-server
 PROJECT_NAME2=sijangtong-client
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> check Server Application pid $PROJECT_NAME"
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk `{print $q}`)
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $q}')
 
 echo "> Server Application Pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
@@ -33,7 +33,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 chmod +x $JAR_NAME
 
 echo "> JAR Name: $JAR_NAME"
-nohup java -jar \
+sudo nohup java -jar \
   -Dspring.config.location=classpath:/application.
 properties,classpath:/application-real.properties \ 
   -Dspring.profiles.active=real \
