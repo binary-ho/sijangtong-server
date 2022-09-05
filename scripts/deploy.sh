14a0274abc1f7da8a5879223e87d76a92838ce20
binary-ho <dfghcvb11@naver.com>
2022-09-05 19:43:49 +0900

    test commit for ci/cd 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ PROJECT_NAME2=sijangtong-client
 sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> check Server Application pid $PROJECT_NAME"
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $q}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar)
 
 echo "> Server Application Pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
@@ -33,11 +33,11 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 chmod +x $JAR_NAME
 
 echo "> JAR Name: $JAR_NAME"
-sudo nohup java -jar \
-  -Dspring.config.location=classpath:/application.
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.
 properties,classpath:/application-real.properties \ 
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 # ,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
 
 echo "> run client project"
@@ -50,8 +50,9 @@ echo "> git pull"
 git pull
 
 echo "> npm build"
-npm install
-npm run build
+# npm install
+# npm run build
+sudo yarn build
 
 echo "> pm2 build"
 pm2 serve build/ 3000 --spa
